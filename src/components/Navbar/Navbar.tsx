111a83565b773f814dadb125714adb5281b88e4e
Gabriel Andreato <gabriel.andreato@minervafoods.com>
2024-09-17 17:13:47 -0300

    Incluido componente de busca

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -5,6 +5,7 @@ import {
     RiShoppingCart2Line,
     RiShoppingCartFill
 } from "react-icons/ri";
+import Busca from "../Busca/Busca";
 
 const iconeProps = {
     color: 'white',
@@ -23,7 +24,7 @@ export default function Navbar() {
                 </div>
             </div>
             <div className={styles.busca}>
-                
+                <Busca/>
             </div>
             <div className={styles.icones}>
                 <a href="/carrinho">
