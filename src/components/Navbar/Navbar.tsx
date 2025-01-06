7eab41e6fd93372e96a335d943d1c3f0eed5fcaf
Gabriel Andreato <gabriel.andreato@minervafoods.com>
2025-01-06 13:46:22 -0300

    Criado Pagina padrao e implementadas rotas

---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,44 @@
+import React from "react";
+import styles from './Navbar.module.scss';
+import {ReactComponent as Logo} from "../../assets/logo.svg";
+import classNames from "classnames";
+import {
+    RiShoppingCart2Line,
+    RiShoppingCartFill
+} from 'react-icons/ri';
+import Busca from "../Busca/Busca";
+
+const iconProps = {
+    color: 'white',
+    size: 24
+}
+
+export default function Navbar() {
+    return (
+        <nav className={styles.nav}>
+            <Logo className={styles.logo}/>
+            <div className={styles.links}>
+                <div>
+                    <a href="/" className={classNames(styles.link, {
+                        [styles.selected]: window.location.hostname === '/'
+                    })}>
+                        Página Inicial
+                    </a>
+                </div>
+            </div>
+            <div className={styles.busca}>
+                <Busca />
+            </div>
+            <div className={styles.icones}>
+                <a href={"/carrinho"}>
+                    {
+                        window.location.pathname === "/carrinho"
+                            ? <RiShoppingCartFill {...iconProps} />
+                            : <RiShoppingCart2Line {...iconProps}/>
+                    }
+
+                </a>
+            </div>
+        </nav>
+    )
+}
