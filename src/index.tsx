2311a86c87e3df2899ff5eea5ad39f447d6ccd86
Gabriel Andreato <gabriel.andreato@minervafoods.com>
2025-01-08 11:54:54 -0300

    setup do redux e criado categorias slice

--- src/index.tsx
+++ src/index.tsx
@@ -2,13 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import Router from "./routes";
+import {Provider} from "react-redux";
+import {store} from "./store/store";
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <Router />
+      <Provider store={store} >
+        <Router />
+      </Provider>
   </React.StrictMode>
 );
 
---
+++ src/store/reducers/ICategorias.ts
@@ -0,0 +1,10 @@
+import eletronicosThumb from "../../assets/categorias/thumbnail/eletronicos.png";
+import eletronicosHeader from "../../assets/categorias/header/eletronicos.png";
+
+export default interface ICategorias {
+    nome: string;
+    thumbnail: string;
+    header: string;
+    id: string;
+    descricao: string
+}
