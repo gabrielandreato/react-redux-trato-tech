f4c07489478a9b741e9005365a5ed2e45a50f41f
Gabriel Andreato <gabriel.andreato@minervafoods.com>
2025-02-26 17:47:38 -0300

    Implementado listeners para busca de categorias

--- src/store/middlewares/categorias.ts
+++ src/store/middlewares/categorias.ts
@@ -1,21 +1,51 @@
 import {createListenerMiddleware} from "@reduxjs/toolkit";
 import {adicionarTodasAsCategorias, carregarCategorias} from "../reducers/categorias";
 import categoriasService from "../../services/categoriaService";
+import {createStandaloneToast} from "@chakra-ui/toast";
 
 
 export const listener = createListenerMiddleware();
+const {toast} = createStandaloneToast()
+
 
 listener.startListening({
     actionCreator: carregarCategorias,
-    effect: async (action, { dispatch, fork}) => {
+    effect: async (action, { dispatch, fork, unsubscribe}) => {
+        toast({
+            title: 'Carregando',
+            description: "Carregando categorias...",
+            duration: 2000,
+            status: 'loading',
+            isClosable: true,
+        })
+
         const tarefa  = fork(async api => {
+            await api.delay(1000)
             return await categoriasService.buscar()
         });
 
         const resposta = await tarefa.result;
 
         if(resposta.status === 'ok'){
-            dispatch(adicionarTodasAsCategorias(resposta.value))
+            toast({
+                title: 'Sucesso!',
+                description: "Categorias carregadas com sucesso!",
+                duration: 2000,
+                status: 'success',
+                isClosable: true,
+            });
+            dispatch(adicionarTodasAsCategorias(resposta.value));
+            unsubscribe();
+        }
+
+        if (resposta.status === 'rejected'){
+            toast({
+                title: 'Erro',
+                description: "Erro na busca de categorias",
+                duration: 2000,
+                status: 'error',
+                isClosable: true,
+            })
         }
 
     }
--- src/store/reducers/categorias.ts
+++ src/store/reducers/categorias.ts
@@ -1,7 +1,6 @@
 import {createAction, createAsyncThunk, createSlice} from "@reduxjs/toolkit";
 import ICategorias from "./interfaces/ICategorias";
 import categoriasService from "../../services/categoriaService";
-import {createStandaloneToast} from "@chakra-ui/toast";
 
 const estadoInicial: ICategorias[] = [];
 
@@ -12,7 +11,6 @@ export const buscarCategorias = createAsyncThunk(
     categoriasService.buscar
 )
 
-const {toast} = createStandaloneToast()
 
 const categoriasSlice = createSlice({
     name: "categorias",
@@ -25,45 +23,6 @@ const categoriasSlice = createSlice({
             return payload;
         }
     },
-    extraReducers: (builder) => {
-        builder.addCase(
-            buscarCategorias.fulfilled,
-            (state, { payload }) => {
-                toast({
-                    title: 'Sucesso!',
-                    description: "Categorias carregadas com sucesso!",
-                    duration: 2000,
-                    status: 'success',
-                    isClosable: true,
-                })
-                return payload;
-            }
-        )
-            .addCase(
-                buscarCategorias.pending,
-                (state, {payload}) => {
-                    toast({
-                        title: 'Carregando',
-                        description: "Carregando categorias...",
-                        duration: 2000,
-                        status: 'loading',
-                        isClosable: true,
-                    })
-                }
-            )
-            .addCase(
-                buscarCategorias.rejected,
-                (state, {payload}) => {
-                    toast({
-                        title: 'Erro',
-                        description: "Erro na busca de categorias",
-                        duration: 2000,
-                        status: 'error',
-                        isClosable: true,
-                    })
-                }
-            )
-    }
 })
 
 export const {adicionarCategorias, adicionarTodasAsCategorias} = categoriasSlice.actions;
